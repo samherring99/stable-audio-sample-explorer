b44d94f2cd7394b8c17847b2b8959a7aaffb5cbe
nightwing <samherring99@gmail.com>
2024-06-14 17:51:38 -0600

    Added debug output statements for TreeModel methods

--- tree_model.py
+++ tree_model.py
@@ -9,6 +9,7 @@ from viz import visualize_tree
 # This class mainly wraps the Stable-Audio-Open model and helper methods with top level params
 class TreeModel:
     def __init__(self):
+        print("Initializing model...")
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
         self.model, self.model_config = get_pretrained_model("stabilityai/stable-audio-open-1.0")
         self.sample_rate = self.model_config["sample_rate"]
@@ -19,23 +20,27 @@ class TreeModel:
 
     # create_node: creates an AudioNode
     def create_node(self, prompt):
+        print("Generating audio for prompt: " + prompt)
         node = create_node(prompt, self.model, self.sample_size, self.sample_rate)
         self.nodes.append(node)
         return node
 
     # remix_node: 'remixes' an AudioNode with a given prompt
     def remix_node(self, node, prompt):
+        print("Remixing {audio} with ".format(audio=node.prompt) + prompt)
         node = remix_node(node, self.model, prompt, self.sample_size, self.sample_rate)
         self.nodes.append(node)
         return node
 
     # combines_nodes: combines two AudioNodes
-    def combine_nodes(self, node1, node2):
-        node = combine_nodes(node1, node2, self.sample_size, self.sample_rate)
+    def combine_nodes(self, node_1, node_2):
+        print("Combining {audio_1} with {audio_2}".format(audio_1=node_1.prompt, audio_2=node_2.prompt))
+        node = combine_nodes(node_1, node_2, self.sample_size, self.sample_rate)
         self.nodes.append(node)
         return node
 
     # visualize: visualize the tree
     def visualize(self):
+        print("Generating visualization...")
         dot = visualize_tree(self.nodes)
         dot.render('tree', format='png', view=True)
