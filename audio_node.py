b0f4b40bb8d9855cbfdd135c4957e7010bba05c4
nightwing <samherring99@gmail.com>
2024-06-14 14:52:02 -0600

    Added remaining pieces to AudioNode class file

--- audio_node.py
+++ audio_node.py
@@ -1,6 +1,8 @@
 from einops import rearrange
 from stable_audio_tools.inference.generation import generate_diffusion_cond
 
+from util import combine_waveforms, format_prompt, save_audio
+
 # AudioNode is an atomic component of the sample exploration tree
 # At its core it stores:
 #   the prompt string 
@@ -73,3 +75,37 @@ class AudioNode:
 
         self.audio = output
         self.file_path = save_audio(output, self.prompt)
+
+# create_node: Creates a node from a given prompt
+# Params: a prompt string to initialize the node and generate 30 audio sample, and the StableAudio model
+# Returns: the initiazlied node
+def create_node(prompt, model, sample_size):
+    node = AudioNode(prompt)
+    node.generate_audio(model, sample_size)
+    return node
+
+# combines_nodes: Combines two existing nodes and adds itself to each of their children
+# Params: Two nodes to combine
+# Returns: the child node of the combined nodes with the combined audio
+def combine_nodes(node_1, node_2):
+    new_child = AudioNode(node_1.prompt + "|" + node_2.prompt)
+    combined_audio = combine_waveforms(node_1.audio, node_2.audio)
+    new_child.audio = combined_audio
+    child = format_prompt(new_child)
+    child.file_path = save_audio(combined_audio, child.prompt)
+
+    node_1.children.append(child)
+    node_2.children.append(child)
+
+    return child
+
+# remix_node: 'Remixes' a given node with a prompt using that node's audio for init
+# Params: a node to remix and a prompt to use for generation
+# Returns: a new child node that has been 'remixed' from the parent node's audio
+def remix_node(node, prompt):
+    remixed_node = AudioNode(node.prompt + "|REMIX|" + prompt)
+    child = format_prompt(remixed_node)
+    child.generate_remixed_audio(prompt, node.audio)
+    node.children.append(child)
+
+    return child
